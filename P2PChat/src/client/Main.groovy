12f6675b8ebfae01bde3d7cca3993570945b74bd
die Sabi <nigistsaba@hotmail.com>
2016-09-12 15:43:47 +0200

    corrects connectToServer-method

--- P2PChat/src/client/Main.groovy
+++ P2PChat/src/client/Main.groovy
@@ -1,14 +1,20 @@
 package client
 
+import client.services.TransportService;
 import groovyx.net.http.RESTClient
 
 class Main {
-	
+
 	static main(args) {
-		
+
+		TransportService transportService = new TransportService()
 		//call to server
-		connectToUserServer("141.45.206:8080")
-		
+		transportService.connectToUserServer("141.45.206:8080")
+
+		ClientServer cl = new ClientServer()
+		cl.startClientServer()
+		println "startet local server..."
+			
 		//read input from console
 		def br = new BufferedReader(new InputStreamReader(System.in))
 		println "Please enter your username: "
--- P2PChat/src/client/services/TransportService.groovy
+++ P2PChat/src/client/services/TransportService.groovy
@@ -2,8 +2,11 @@ package client.services
 
 import groovyx.net.http.RESTClient
 
+import javax.ws.rs.PathParam
 
-boolean connectToUserServer(def inetAddr) {
+
+
+boolean connectToUserServer(@PathParam ("inetAddr")def inetAddr) {
 	
 	RESTClient client = new RESTClient('http://${inetAddr}:8080/')
 	def response = client.get(path: '/resource')
