9b4912a695a0f17ac11741be65d2454febabfea2
Suzanne <suka@posteo.de>
2016-09-12 11:57:39 +0200

    connects to server

--- P2PChat/src/client/Main.groovy
+++ P2PChat/src/client/Main.groovy
@@ -1,17 +1,22 @@
 package client
 
+import groovyx.net.http.RESTClient
+
 class Main {
 	
 	static main(args) {
-		def actualUser = new User()
-		println "Your IP-address is: ${actualUser.ipAddress}"
+		
+		//call to server
+		RESTClient client = new RESTClient('http://141.45.211.14:8080/')
+		def response = client.get(path: '/resource')
+		assert response.status == 200
+		assert response.data == 'Hello Server'
 		
 		//read input from console
 		def br = new BufferedReader(new InputStreamReader(System.in))
 		println "Please enter your username: "
 		def userName = br.readLine()
-		actualUser.name = userName
-		println "Welcome to BChatty $actualUser.name!"
+		println "Welcome to MESSAS $userName!"
 	}
 
 }
