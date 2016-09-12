225667d755d60e701d663709060595241d5fefc7
Elif Yavuz <s0550567@htw-berlin.de>
2016-09-12 17:01:06 +0200

    Umsetzung von Chatablauf/ Startprogramm Hallo (Elif, Silvia)

--- P2PChat/src/client/Main.groovy
+++ P2PChat/src/client/Main.groovy
@@ -6,6 +6,13 @@ class Main {
 	
 	static main(args) {
 		
+		def zahl = 1
+//		List list = new ArrayList<String>()
+//		list.add("Silvia")
+//		list.add("Elif")
+//		list.add("Maryna")
+		
+		
 		//call to server
 		RESTClient client = new RESTClient('http://141.45.211.14:8080/')
 		def response = client.get(path: '/resource')
@@ -14,9 +21,26 @@ class Main {
 		
 		//read input from console
 		def br = new BufferedReader(new InputStreamReader(System.in))
-		println "Please enter your username: "
+		println "Welcome to MESSAS-Chat! \nPlease enter your nickname: "
 		def userName = br.readLine()
-		println "Welcome to MESSAS $userName!"
+		println "Please enter your password: "
+		def userPwd = br.readLine()     //todo: hide password
+		println "Hello " + userName + "! You are now in your personally chatroom. "
+		
+		
+		
+		if(zahl == 1){
+			println "These friends are available:\n With wich friend do you want to chat? \n (0)logout" 
+		}
+		else{
+			println "I´m sorry, it´s seems that all your friends are busy :( \n (0)logout"
+		}
+		
+		def eingabe = br.readLine()
+		
+		if(eingabe == "0"){
+			println "Bye Bye"
+		}
 	}
 
 }
