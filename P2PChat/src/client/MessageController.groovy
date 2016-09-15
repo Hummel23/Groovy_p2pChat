1e96bd8f2496a1bf62cb9bd37604f13349d80e76
Suzanne <suka@posteo.de>
2016-09-15 10:14:46 +0200

    sends message as json and prints it nicely

--- P2PChat/src/client/MessageController.groovy
+++ P2PChat/src/client/MessageController.groovy
@@ -1,12 +1,11 @@
 package client
 
-import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
 
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
-import javax.ws.rs.core.MediaType
 
 /**
  * 
@@ -24,12 +23,20 @@ class Root {
 class Chat {
 	
 	@GET
-	@Produces([MediaType.APPLICATION_JSON])
+//	@Produces([MediaType.APPLICATION_JSON])
 	public void getTextMessage(
 		@PathParam("message") String message) {
-		println message
-		new JsonBuilder(message)
-		.toPrettyString()
+		def json = new JsonSlurper().parseText(message)
+		println messageToString(json)
+	}
+		
+	String messageToString(def message) {
+		return """
+		============================
+		${message.sender.toUpperCase()}:
+		----------------------------
+		${message.content}
+		============================"""
 	}
 }
 
--- P2PChat/src/client/Sender.groovy
+++ P2PChat/src/client/Sender.groovy
@@ -1,19 +1,51 @@
 package client
 
 import groovyx.net.http.RESTClient
-
+import client.services.MessageService
 
 @Singleton
 class Sender {
 
 	def name
 	RESTClient client
+	def messageService = MessageService.instance
 
 	public void sendMessage(String msg) {
 //		def ip = getChatPartnerIP(msg.chatPartnerID)
-		def ip = InetAddr.getChatPartnerInetAddr()
+		def ip = InetAddr.ChatPartnerInetAddr
+		println "ip: $ip"
+		Message messageObject = new TextMessage(msg, this.name, "chatPartnerID")
+		def msgJson = messageService.convertToJSON(messageObject)	
+		println "JSON: "+msgJson
 		client = new RESTClient("http://${ip}:8080/")
-		def response = client.get(path: "/${msg}")
+		def response = client.get(path: "/${msgJson}")
+		println "resp: "+ response.data
+		println "status: "+ response.status
+		
+		//-----
+//		@Produces([MediaType.APPLICATION_JSON])
+//		def getList(){
+//			def list = Login.userListe
+//			return new JsonBuilder(list)
+//					.toPrettyString()
+//		}
+
+		//-----
+//		http.request(POST) {
+//			uri.path = 'http://example.com/handler.php'
+//			body = [name: 'bob', title: 'construction worker']
+//			requestContentType = ContentType.JSON
+//		
+//			response.success = { resp ->
+//				println "Success! ${resp.status}"
+//			}
+//		
+//			response.failure = { resp ->
+//				println "Request failed with status ${resp.status}"
+//			}
+//		}
+		
+		
 //		assert response.status == 200
 //		assert response.data == 'Hello Server'
 //		println response.data.toString()
