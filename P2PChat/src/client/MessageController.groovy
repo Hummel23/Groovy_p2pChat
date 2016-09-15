fdd69c596863aa8fb37f76cad20c02268b542f1a
Suzanne <suka@posteo.de>
2016-09-15 09:36:38 +0200

    produces json on server-side

--- P2PChat/src/client/MessageController.groovy
+++ P2PChat/src/client/MessageController.groovy
@@ -1,8 +1,12 @@
 package client
 
+import groovy.json.JsonBuilder
+
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
 
 /**
  * 
@@ -12,7 +16,7 @@ class Root {
 	
 	@GET
 	public String startServer() {
-		return "(0)client server up..."
+		return "client server up..."
 	}
 }
 
@@ -20,9 +24,12 @@ class Root {
 class Chat {
 	
 	@GET
+	@Produces([MediaType.APPLICATION_JSON])
 	public void getTextMessage(
 		@PathParam("message") String message) {
-
+		println message
+		new JsonBuilder(message)
+		.toPrettyString()
 	}
 }
 
